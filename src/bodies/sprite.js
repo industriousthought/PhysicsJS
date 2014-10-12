d0a6e8b79f38a24bd13e7f8781d765c2ef3ecf08
Brian Blackwell <industriousthought@gmail.com>
2014-10-12 00:29:53 +0000

    Started fleshing out my spite body with an animation function that will handle animating the body with different segments of the texture map.

---
+++ src/bodies/sprite.js
@@ -0,0 +1,49 @@
+    /** 
+     * @requires geometries/circle
+     */
+    /** 
+     * class CircleBody < Body
+     *
+     * Physics.body('circle')
+     *
+     * The circle body has a circular shape.
+     *
+     * Additional options include:
+     * - radius: the radius
+     *
+     * Example:
+     *
+     * ```javascript
+     * var round = Physics.body('circle', {
+     *     x: 30,
+     *     y: 20,
+     *     radius: 5
+     * });
+     * ```
+     **/
+    Physics.body('sprite', 'circle', function( parent ){
+
+        return {
+
+            init: function( options ){
+
+                parent.init.call(this, options);
+
+            },
+
+            animate: function() {
+                console.log('animate');
+                if (this.view) {
+                    if (this.texture) {
+                        
+
+                    } else {
+                        this.texture = this.view;
+                    }
+                }
+
+            }
+
+        };
+    });
+
