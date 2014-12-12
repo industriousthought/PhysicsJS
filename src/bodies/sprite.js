5d36142144aa38c0652dc5f4edf23c2c6bf7525f
Brian Blackwell <industriousthought@gmail.com>
2014-12-12 19:48:41 +0000

    do over

--- src/bodies/sprite.js
+++ src/bodies/sprite.js
@@ -29,62 +29,9 @@
 
                 parent.init.call(this, options);
                 
-                console.log(this.view);
-
-            },
-
-            animate: function() {
-                var ctx, w, h, makeTexture;
-
-                makeTexture = function(c) {
-                    c.drawImage(this.view, 0, 0, this.view.width, this.view.height);
-                    this.view = document.createElement('canvas');
-                    this.view.width = this.texture.width / Math.max.apply(0, this.texMap.x);
-                    this.view.height = this.texture.height / this.texMap.y;
-
-
-                };
-
-                if (this.view) {
-                    if (this.texture) {
-                        if (this.view.getContext) {
-                            if (this.texMap.tick < 10) {
-                                this.texMap.tick += 1;
-                            } else {
-                                w = this.view.width;
-                                h = this.view.height;
-                                if (this.texMap.frame < this.texMap.x[this.texMap.activity] - 1) {
-                                    this.texMap.frame += 1;
-                                } else {
-                                    this.texMap.frame = 0;
-                                }
-                                this.texMap.tick = 0;
-                                ctx = this.view.getContext("2d");
-                                this.view.width = this.view.width;
-
-                        
-                                ctx.drawImage(this.texture,
-                                    this.texMap.frame * w, 
-                                    this.texMap.activity * h,
-                                    w,
-                                    h,
-                                    0, 
-                                    0,
-                                    w * this.texMap.scaler,
-                                    h * this.texMap.scaler);
-                            }
-                        }
-                    } else {
-                        this.texture = document.createElement('canvas');
-                        ctx = this.texture.getContext("2d");
-                        this.texture.height = this.view.height;
-                        this.texture.width = this.view.width;
-                        this.texMap.tick = 0;
-                        this.view.onload = makeTexture.bind(this, ctx);
-                    }
-                }
 
             }
+
 
         };
     });
--- src/renderers/canvas.js
+++ src/renderers/canvas.js
@@ -602,8 +602,55 @@ Physics.renderer('canvas', function( proto ){
             if ( styles.src ){
                 view = new Image();
                 view.addEventListener('load', function() {
-                    console.log('load');
-                });
+
+                    var texture, w, h, makeTexture, tick, view;
+
+                    if (this.options.texMap) {{
+                        this.texture = document.createElement('canvas');
+                        texture = this.texture.getContext("2d");
+                        this.texture.height = this.view.height;
+                        this.texture.width = this.view.width;
+                        tick = 0;
+                        texture.drawImage(this.view, 0, 0, this.view.width, this.view.height);
+                        this.view = document.createElement('canvas');
+                        this.view.width = this.texture.width / Math.max.apply(0, this.texMap.x);
+                        this.view.height = this.texture.height / this.texMap.y;
+                        view = this.view.getContext("2d");
+
+
+
+                    
+                        this.on('animateSprite', function() {
+
+
+                            if (tick < 10) {
+                                tick += 1;
+                            } else {
+                                w = this.view.width;
+                                h = this.view.height;
+                                if (this.texMap.frame < this.texMap.x[this.texMap.activity] - 1) {
+                                    this.texMap.frame += 1;
+                                } else {
+                                    this.texMap.frame = 0;
+                                }
+                                tick = 0;
+                                this.view.width = this.view.width;
+
+                        
+                                view.drawImage(this.texture,
+                                    this.texMap.frame * w, 
+                                    this.texMap.activity * h,
+                                    w,
+                                    h,
+                                    0, 
+                                    0,
+                                    w * this.texMap.scaler,
+                                    h * this.texMap.scaler);
+                            }
+                        }
+                    }
+
+                }.bind(this));
                 view.src = styles.src;
                 if ( styles.width ){
                     view.width = styles.width;
